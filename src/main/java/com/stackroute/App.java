fa8f4086bd14d2d46b7241884c677e1cb8d04df7
shiv.prakash <shiv.prakash@cgi.com>
2019-07-31 12:04:03 +0530

    jdbc demo

---
+++ src/main/java/com/stackroute/App.java
@@ -0,0 +1,14 @@
+package com.stackroute;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        JdbcBasicDemo jdbc=new JdbcBasicDemo();
+        jdbc.displayUser();
+    }
+}
